e7b3e8db060687dd89f8b05f6e6c340c97f550f4
gmoty <pro.gregoire.moty@gmail.com>
2015-10-22 11:56:50 +0200

    Fix regex

--- MatchUp/js.js
+++ MatchUp/js.js
@@ -1,7 +1,7 @@
 (function (d,w) {
     var links = ['http://domain.com/path/to/file.txt', 'ftp://domain.com/file.psd', 'git://user:password@git.server/tree/export.json', 'ssh://user@path.co/var/www/'],
         text = ['Url : ', 'Protocole : ', 'User : ', 'Password : ', 'Domain : ', 'Path : '],
-        regex = /([a-z]+)+(?::\/\/)+([a-z0-9._-]+)+(?:\.|:|@)+([a-z]+)+(\/|@|\.)+([a-z0-9._/-]+)/,
+        regex = /([a-z\/]+)+(?::\/\/)+([a-z0-9._-]+)+(?:\.|:|@)+([a-z]+)+(\/|@|\.)+([a-z0-9._/-]+)/,
         body = d.getElementById('data'),
         p, data, match;
 
