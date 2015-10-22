c6dc4df1dfb694a868f7d589f4a08ce4bc693b30
gmoty <pro.gregoire.moty@gmail.com>
2015-10-22 17:11:59 +0200

    Test mode

--- MatchUp/js.js
+++ MatchUp/js.js
@@ -1,18 +1,27 @@
 (function (d,w) {
-    var links = ['http://domain.com/path/to/file.txt', 'ftp://domain.com/file.psd', 'git://user:password@git.server/tree/export.json', 'ssh://user@path.co/var/www/'],
+    var links = ['http://domain.com/path/to/file.txt', 'ftp://domain.com/file.psd', 'git://user:password@git.server/tree/export.json', 'ssh://user@path.co/var/www/', '//domain.com'],
         text = ['Url : ', 'Protocole : ', 'User : ', 'Password : ', 'Domain : ', 'Path : '],
         regex = /([a-z\/]+)+(?::\/\/)+([a-z0-9._-]+)+(?:\.|:|@)+([a-z]+)+(\/|@|\.)+([a-z0-9._/-]+)/,
         body = d.getElementById('data'),
         p, data, match;
 
+    var protocol = '([a-z\/]+)',
+        separator = '(?::\/\/)',
+        regex2 = '/' + protocol + '/',
+        match2;
+    console.log(regex2);
+
     for(var a = 0, b = links.length; a < b; a++){
         for(var i = 0, c = text.length; i < c; i++){
             p = d.createElement('p');
-            match = links[a].match(regex);
+            match2 = links[a].match(regex2);
             data = d.createTextNode(text[i].concat(match[i]));
             p.appendChild(data);
             body.appendChild(p);
         }
     }
+
+
+
 
 })(document, window);
