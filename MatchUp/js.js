d675c3b6b35eaa0e485a6906fada955689602199
gmoty <pro.gregoire.moty@gmail.com>
2015-10-22 11:47:54 +0200

    Add new regex method

--- MatchUp/js.js
+++ MatchUp/js.js
@@ -1,7 +1,18 @@
-var link = prompt("Enter url to match!");
+(function (d,w) {
+    var links = ['http://domain.com/path/to/file.txt', 'ftp://domain.com/file.psd', 'git://user:password@git.server/tree/export.json', 'ssh://user@path.co/var/www/'],
+        text = ['Url : ', 'Protocole : ', 'User : ', 'Password : ', 'Domain : ', 'Path : '],
+        regex = /([a-z]+)+(?::\/\/)+([a-z0-9._-]+)+(?:\.|:|@)+([a-z]+)+(\/|@|\.)+([a-z0-9._/-]+)/,
+        body = d.getElementById('data'),
+        p, data, match;
 
-if(/(https|http|ftp|steam|git|ssh):\/\/+([a-z0-9._-]+)+(\.|:|@)+([a-z]+)+(\/|@|\.)+([a-z0-9._/-]+)/.test(link)){
-    console.log('Find !' + '\nProtocole : ' + RegExp.$1 + '\nLocal : ' + RegExp.$2 + '\nDomaine : ' + RegExp.$4 + '\npath :' + RegExp.$6);
-}else{
-    console.log('Don\'t find');
-}
+    for(var a = 0, b = links.length; a < b; a++){
+        for(var i = 0, c = text.length; i < c; i++){
+            p = d.createElement('p');
+            match = links[a].match(regex);
+            data = d.createTextNode(text[i].concat(match[i]));
+            p.appendChild(data);
+            body.appendChild(p);
+        }
+    }
+
+})(document, window);
