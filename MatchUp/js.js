d83f918c154b2a068e59e92219e3fa0d76083d72
gmoty <pro.gregoire.moty@gmail.com>
2015-10-23 13:45:32 +0200

    Fix

--- MatchUp/js.js
+++ MatchUp/js.js
@@ -1,17 +1,16 @@
 (function (d, w) {
     var links = ['http://domain.com/path/to/file.txt', 'ftp://domain.com/file.psd', 'git://user:password@git.server/tree/export.json', 'ssh://user@path.co/var/www/', '//domain.com'],
         body = d.getElementById('data'),
-        data, regex, protocol, separator, domain, extension, char, charGit, match;
+        data, regex, protocol, separator, domain, char, charGit, match;
 
     protocol = '([a-z0-9\/]+)';
-    separator = '([\/\/:\.@]*)';
-    domain = '([a-z0-9\/]+)';
-    extension = '([a-z\.]*)';
+    separator = '(?:[\/\/:\.@]*)';
+    domain = '([a-z0-9\.]+)';
     char = '([a-z0-9\/\.]*)';
     charGit = '([a-z0-9]*)';
 
 
-    regex = new RegExp(protocol + separator + domain + separator + charGit + extension + char + separator + charGit + separator + charGit + separator + char);
+    regex = new RegExp(protocol + separator + domain + separator  + charGit + char + separator + char);
 
     for (var i = 0, o = links.length; i < o; i++) {
         match = links[i].match(regex);
@@ -22,8 +21,6 @@
             p.appendChild(data);
             body.appendChild(p);
         }
-
-
     }
 
 })(document, window);
