13a815a4dd310e05b24aaaaac72979ac45cff87e
gmoty <pro.gregoire.moty@gmail.com>
2015-10-23 10:59:54 +0200

    New version

--- MatchUp/js.js
+++ MatchUp/js.js
@@ -1,27 +1,27 @@
-(function (d,w) {
+(function (d, w) {
     var links = ['http://domain.com/path/to/file.txt', 'ftp://domain.com/file.psd', 'git://user:password@git.server/tree/export.json', 'ssh://user@path.co/var/www/', '//domain.com'],
-        text = ['Url : ', 'Protocole : ', 'User : ', 'Password : ', 'Domain : ', 'Path : '],
-        regex = /([a-z\/]+)+(?::\/\/)+([a-z0-9._-]+)+(?:\.|:|@)+([a-z]+)+(\/|@|\.)+([a-z0-9._/-]+)/,
         body = d.getElementById('data'),
-        p, data, match;
+        data, regex, protocol, separator, domain, extension, char, charGit, match;
 
-    var protocol = '([a-z\/]+)',
-        separator = '(?::\/\/)',
-        regex2 = '/' + protocol + '/',
-        match2;
-    console.log(regex2);
+    protocol = '([a-z0-9\/]+)';
+    separator = '([\/\/:\.@]*)';
+    domain = '([a-z0-9\/]+)';
+    extension = '([a-z\.]*)';
+    char = '([a-z0-9\/\.]*)';
+    charGit = '([a-z0-9]*)';
 
-    for(var a = 0, b = links.length; a < b; a++){
-        for(var i = 0, c = text.length; i < c; i++){
+
+    regex = new RegExp(protocol + separator + domain + separator + charGit + extension + char + separator + charGit + separator + charGit + separator + char);
+
+    for (var i = 0, o = links.length; i < o; i++) {
+        match = links[i].match(regex);
+        for (var s = 0, q = match.length; s < q; s++) {
             p = d.createElement('p');
-            match2 = links[a].match(regex2);
-            data = d.createTextNode(text[i].concat(match[i]));
+            data = d.createTextNode(match[s]);
+            console.log(match[s]);
             p.appendChild(data);
             body.appendChild(p);
         }
     }
-
-
-
 
 })(document, window);
