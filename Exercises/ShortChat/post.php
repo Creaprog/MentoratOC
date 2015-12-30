5e4f06745a45f1542ad55e9d5f8d5102f06edacf
Grégoire Moty <pro.gregoire.moty@gmail.com>
2015-12-30 10:36:31 +0100

    Add session, verify if the pseudo and message is not empty in the form, protection anti xss and create session['pseudo']

--- Exercises/ShortChat/post.php
+++ Exercises/ShortChat/post.php
@@ -1,12 +1,21 @@
 <?php
-try {
-    $bdd = new PDO('mysql:host=localhost;dbname=chat;charset=utf8', 'root', '');
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    die('Error : ' . $e->getMessage());
+session_start();
+if(!empty($_POST['pseudo']) AND !empty($_POST['message'])){
+    $author = htmlspecialchars($_POST['pseudo']);
+    $message = htmlspecialchars($_POST['message']);
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=chat;charset=utf8', 'root', '');
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (Exception $e) {
+        die('Error : ' . $e->getMessage());
+    }
+
+    $req = $bdd->prepare('INSERT INTO messages (author, message, moment) VALUES(?, ?, NOW())');
+    $req->execute(array($author, $message));
+
+    $_SESSION['pseudo'] = $author;
+
+    header('Location: index.php');
+}else{
+    echo 'Thank to complete the form before click on submit button';
 }
-
-$req = $bdd->prepare('INSERT INTO messages (author, message, moment) VALUES(?, ?, NOW())');
-$req->execute(array($_POST['pseudo'], $_POST['message']));
-
-header('Location: index.php');
