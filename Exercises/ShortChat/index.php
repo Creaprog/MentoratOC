e2ff696a77423c29fa65e5860e4e1dc6e0f413b5
Grégoire Moty <pro.gregoire.moty@gmail.com>
2016-01-01 18:54:50 +0100

    Add commentary

--- Exercises/ShortChat/index.php
+++ Exercises/ShortChat/index.php
@@ -13,7 +13,9 @@ session_start();
 <form action="post.php" method="post">
     <p>
         <label for="pseudo">Pseudo :</label>
-        <input type="text" name="pseudo" id="pseudo" value="<?php if(!empty($_SESSION['pseudo'])){echo $_SESSION['pseudo'];} ?>"/>
+        <input type="text" name="pseudo" id="pseudo" value="<?php if (!empty($_SESSION['pseudo'])) {
+            echo $_SESSION['pseudo'];
+        } ?>"/>
         <br/>
         <label for="message">Message :</label>
         <input type="text" name="message" id="message"/>
@@ -24,13 +26,16 @@ session_start();
 
 <?php
 try {
+    //Connexion with bdd
     $bdd = new PDO('mysql:host=localhost;dbname=chat;charset=utf8', 'root', '');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
+    //Get error
     die('Error : ' . $e->getMessage());
 }
 $resp = $bdd->query('SELECT author, message, moment FROM messages ORDER BY moment DESC LIMIT 0, 10');
 while ($message = $resp->fetch()) {
+    //Print the result of query and re write date in french
     echo '<p>Le ' . date("d/m/Y à H:i ", strtotime($message['moment'])) . '<strong>' . htmlspecialchars($message['author']) . '</strong> : ' . htmlspecialchars($message['message']) . '</p>';
 }
 
--- Exercises/ShortChat/post.php
+++ Exercises/ShortChat/post.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-if(isset($_POST['pseudo'], $_POST['message'])&&$_POST['pseudo']&&$_POST['message']){
+if (isset($_POST['pseudo'], $_POST['message']) && $_POST['pseudo'] && $_POST['message']) {
+    //Protection of xss
     $author = htmlspecialchars($_POST['pseudo']);
     $message = htmlspecialchars($_POST['message']);
     try {
@@ -9,13 +10,17 @@ if(isset($_POST['pseudo'], $_POST['message'])&&$_POST['pseudo']&&$_POST['message
     } catch (Exception $e) {
         die('Error : ' . $e->getMessage());
     }
-
+    //Prepare query
     $req = $bdd->prepare('INSERT INTO messages (author, message, moment) VALUES(?, ?, NOW())');
+    //Execute query with protected values
     $req->execute(array($author, $message));
 
+    //Define the pseudo session
     $_SESSION['pseudo'] = $author;
 
+    //Redirect to chat
     header('Location: index.php');
-}else{
+} else {
+    //Else error print a error message
     echo 'Thank to complete the form before click on submit button';
 }
