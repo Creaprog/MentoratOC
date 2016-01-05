ac34730d0a39cfacf3627baa25b35183aa02c535
Grégoire Moty <pro.gregoire.moty@gmail.com>
2016-01-05 16:25:58 +0100

    Fix missing dot

--- Exercises/ContactManager/js/contacts.js
+++ Exercises/ContactManager/js/contacts.js
@@ -5,7 +5,7 @@ var tableauContact = [];
 /* Create contact object for facilitate creation of on contact */
 var Contact = {
     /* initialization of contact and push in into array */
-    initContact: function(nom, prenom){
+    initContact: function (nom, prenom) {
         this.nom = nom;
         this.prenom = prenom;
         tableauContact.push(this);
@@ -20,27 +20,28 @@ Carole.initContact('Lévisse', 'Carole');
 /* Creation of second contact */
 var Melodie = Object.create(Contact);
 /* Initialization of Melodie */
-Melodie.initContact('Nelsonne', 'Mélodie')
+Melodie.initContact('Nelsonne', 'Mélodie');
 
 /* Default text */
 console.info("Bienvenue dans le gestionnaire des contacts !" + "\n" + options);
 /* Creation of loop and seized variable declaration */
 var saisi, nomContact, prenomContact;
-do{
+do {
     saisi = Number(prompt("Choisissez une option :"));
-    if(saisi == 1){
-        console.info("Voici la liste de tous vos contacts :")
-        for(var i = 0; i < tableauContact.length; i++){
+    if (saisi == 1) {
+        console.info("Voici la liste de tous vos contacts :");
+        for (var i = 0; i < tableauContact.length; i++) {
             console.log("Nom : " + tableauContact[i].nom + ", prénom : " + tableauContact[i].prenom);
         }
         console.info(options);
-    }else if(saisi == 2){
+    } else if (saisi == 2) {
         nomContact = prompt("Entrez le nom du nouveau contact :");
         prenomContact = prompt("Entrez le prenom du nouveau contact :");
         var nouveauContact = Object.create(Contact);
         nouveauContact.initContact(nomContact, prenomContact);
         console.info("Le nouveau contact a été ajouté" + "\n" + options);
     }
-}while(saisi != 0){
+} while (saisi != 0)
+{
     console.info("Au revoir");
 }
