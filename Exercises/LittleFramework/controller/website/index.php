3be549351fc40d7427c3356ec698c616e31955fa
Grégoire Moty <pro.gregoire.moty@gmail.com>
2016-01-09 23:38:23 +0100

    Add post action to use add_new model and protect over xss

--- Exercises/LittleFramework/controller/website/index.php
+++ Exercises/LittleFramework/controller/website/index.php
@@ -1,5 +1,16 @@
 <?php
 include_once "model/website/get_news.php";
+if(isset($_POST['title'], $_POST['text'], $_POST['author']) && $_POST['title'] && $_POST['text'] && $_POST['author'])
+{
+    include_once "model/website/add_new.php";
+
+    $title = htmlspecialchars($_POST['title']);
+    $text = htmlspecialchars($_POST['text']);
+    $author = htmlspecialchars($_POST['author']);
+
+    add_new($title, $text, $author);
+
+}
 $news = get_news(0, 5);
 
 foreach ($news as $key => $new) {
