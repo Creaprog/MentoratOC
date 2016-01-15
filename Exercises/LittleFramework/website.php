23173a18edef1115fabd2c2be1809f16b765b4e1
Grégoire Moty <pro.gregoire.moty@gmail.com>
2016-01-15 17:05:39 +0100

    Add routing

--- Exercises/LittleFramework/website.php
+++ Exercises/LittleFramework/website.php
@@ -1,6 +1,5 @@
 <?php
 const ROOT = __DIR__;
 include_once ROOT . '/config/db.php';
-if (!isset($_GET['section']) || $_GET['section'] == 'index') {
-    include_once ROOT . '/controller/website/index.php';
-}
+include_once ROOT . '/router/router.php';
+run($_GET['url']);
