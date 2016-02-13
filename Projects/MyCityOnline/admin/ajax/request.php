453f0ba73e72848d1be54c9f811f181a8914040e
gmoty <pro.gregoire.moty@gmail.com>
2016-02-13 20:00:57 +0100

    Add request file for ajax

---
+++ Projects/MyCityOnline/admin/ajax/request.php
@@ -0,0 +1,30 @@
+<?php
+if (isset($_POST['title'], $_POST['image'], $_POST['content'], $_POST['type']) && $_POST['title'] && $_POST['image'] && $_POST['content'] && $_POST['type']) {
+    $title = htmlspecialchars($_POST['title']);
+    $image = htmlspecialchars($_POST['image']);
+    $content = htmlspecialchars($_POST['content']);
+    $type = htmlspecialchars($_POST['type']);
+
+    switch ($type) {
+        case "addNew":
+            require_once '../functions/add_new.php';
+            try {
+                add_new($title, $image, $content);
+                $result = 'requestOk';
+            } catch (Exception $e) {
+                $result = $e;
+            }
+            echo $result;
+            break;
+        case "addInfo":
+            require_once '../functions/add_info.php';
+            try {
+                add_info($title, $image, $content);
+                $result = 'requestOk';
+            } catch (Exception $e) {
+                $result = $e;
+            }
+            echo $result;
+            break;
+    }
+}
