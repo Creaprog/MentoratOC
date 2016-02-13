cd3f12be89b940e0ccb5f2366ef40d487c214acb
gmoty <pro.gregoire.moty@gmail.com>
2016-02-13 14:25:22 +0100

    Add get_news function

---
+++ Projects/MyCityOnline/functions/get_news.php
@@ -0,0 +1,21 @@
+<?php
+function get_news($offset, $limit)
+{
+
+    global $bdd;
+
+    $offset = (int)$offset;
+    $limit = (int)$limit;
+
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $req = $bdd->prepare('SELECT id, title, content, image FROM news ORDER BY ID DESC LIMIT :offset, :limit');
+    $req->bindParam(':offset', $offset, PDO::PARAM_INT);
+    $req->bindParam(':limit', $limit, PDO::PARAM_INT);
+    $req->execute();
+
+    $news = $req->fetchAll();
+
+    return $news;
+
+}
