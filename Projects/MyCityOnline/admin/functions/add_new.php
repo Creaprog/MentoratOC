1127302b2f67305097ca9e456021858a9cd7af08
gmoty <pro.gregoire.moty@gmail.com>
2016-02-13 20:02:25 +0100

    Add ajax functions

---
+++ Projects/MyCityOnline/admin/functions/add_new.php
@@ -0,0 +1,15 @@
+<?php
+require_once '../config/db.php';
+function add_new($title, $image, $content)
+{
+    global $bdd;
+
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $req = $bdd->prepare('INSERT INTO news VALUES ("", :title, :content, :image)');
+    $req->bindParam(':title', $title, PDO::PARAM_STR);
+    $req->bindParam(':content', $content, PDO::PARAM_STR);
+    $req->bindParam(':image', $image, PDO::PARAM_STR);
+
+    $req->execute();
+}
