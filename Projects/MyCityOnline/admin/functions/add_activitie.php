57d2a06e5a2a15ccd93fb9864b282b0c5c6f80c7
gmoty <pro.gregoire.moty@gmail.com>
2016-02-13 20:23:12 +0100

    Add add activitie function

---
+++ Projects/MyCityOnline/admin/functions/add_activitie.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../config/db.php';
+function add_activitie($title, $image, $content)
+{
+    global $bdd;
+
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $req = $bdd->prepare('INSERT INTO activities VALUES ("", :title, :image, :content)');
+    $req->bindParam(':title', $title, PDO::PARAM_STR);
+    $req->bindParam(':image', $image, PDO::PARAM_STR);
+    $req->bindParam(':content', $content, PDO::PARAM_STR);
+
+
+    $req->execute();
+}
