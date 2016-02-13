94a3d7db318c9c3401d0ef13bfd720ee06318a74
gmoty <pro.gregoire.moty@gmail.com>
2016-02-13 16:05:45 +0100

    Add contact page

---
+++ Projects/MyCityOnline/contact.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width"/>
+    <link rel="stylesheet" href="css/styles.css"/>
+    <!--[if lte IE 8]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <!--[if lte IE 7]>
+    <body class="ie7">
+    <![endif]-->
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+    <title>My City Online</title>
+</head>
+<body>
+<div id="block_body">
+    <?php include('header.php'); ?>
+
+    <section>
+        <form method="post" action="" class="contact">
+            <p>
+                <label for="title">Titre du message :</label>
+                <input type="text" name="title" id="title"/>
+
+                <br/>
+
+                <label for="name">Votre nom :</label>
+                <input type="text" name="name" id="name"/>
+
+                <br/>
+
+                <label for="born">Votre date de naissance :</label>
+                <input type="date" name="born" id="born"/>
+
+                <br/>
+
+                <label for="email">Votre e-mail :</label>
+                <input type="email" name="email" id="email"/>
+
+                <br/>
+
+                <input type="checkbox" name="citizen" id="citizen"/>
+                <label for="citizen">Résident de la ville</label>
+
+                <br/>
+
+                <label for="nationality">Nationalité</label>
+                <select name="nationality" id="nationality">
+                    <option value="Fraçaise">Française</option>
+                    <option value="Marocaine">Marocaine</option>
+                    <option value="Algérienne">Algérienne</option>
+                    <option value="Américaine">Américaine</option>
+                    <option value="Anglaise">Anglaise</option>
+                    <option value="Allemande">Allemande</option>
+                </select>
+
+                <br/>
+
+                <label for="message">Message</label>
+            <textarea name="message" id="message">
+
+            </textarea>
+            </p>
+        </form>
+    </section>
+
+    <footer>
+        <a href="#">Administration</a>
+    </footer>
+
+</div>
+</body>
+</html>
