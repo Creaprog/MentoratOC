e6c25f0ad0dbc7328822fab00162421945e79ec5
gmoty <pro.gregoire.moty@gmail.com>
2016-05-13 14:47:13 +0200

    Fix commentary

--- Exercises/LittleFrameworkPoo/controller/RouterController.php
+++ Exercises/LittleFrameworkPoo/controller/RouterController.php
@@ -26,12 +26,12 @@ class RouterController
         // Ici ma logique est la suivante, je parcours toutes mes routes, et pour chaque route j'execute une regex pour voir si l'uri match avec une des key d'une route, si elle match alors
         // On l'execute et si dans la key on a une regex avec le match precedant on retrouve sa valeur
         // Sauf que deja la premiere route va matcher tout les temps (/, /test, /toto) et ensuite je peux pas recuperer la regex de la routes du coup je suis bloquer
-        
+
         foreach ($this->routes as $key => $route) {
             $regex = str_replace('/', '\/', $key);
 
             if (preg_match('/' . $regex . '/', $uri, $matched)) {
-                $action = explode("::", $route);
+                //$action = explode("::", $route);
                 //call_user_func(__NAMESPACE__. '\\' . $route);
                 // NewsController::index();
                 echo 'ok';
